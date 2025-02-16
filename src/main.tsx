15dd18e020d4970a97adfdac5ae3cd56d5f15bef
Matt7474 <dimier.matt@gmail.com>
2025-02-16 15:13:38 +0100

    modiif

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-const rootElem = document.getElementById('root');
-
-if (rootElem) {
-  createRoot(rootElem).render(
-    <App />,
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <App />,
+);
