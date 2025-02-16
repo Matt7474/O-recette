17ad89087c786785dcb4e32783f240378c29211b
Matt7474 <dimier.matt@gmail.com>
2025-02-16 10:05:42 +0100

    modif main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { BrowserRouter } from 'react-router';
 
 const rootElem = document.getElementById('root');
 
 if (rootElem) {
   createRoot(rootElem).render(
-    <App />,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   );
 }
