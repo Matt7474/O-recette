1b01c9c284abf52e594d554942eaeedb637a798d
Matt7474 <dimier.matt@gmail.com>
2025-02-18 22:14:25 +0100

    test surge deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-const base = process.env.GITHUB_ACTIONS ? './orecette/' : './';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
